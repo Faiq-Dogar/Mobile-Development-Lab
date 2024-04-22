7c75b93738f38d1a08fc2ac2502a7d1afed81ae4
Faiq Dogar <136640929+Faiq-Dogar@users.noreply.github.com>
2024-04-22 11:45:07 +0500

    user location

--- PMD_SE_A_Java/app/src/main/java/com/example/pmd_se_a_java/GoogleMapAPI/MapsActivity.java
+++ PMD_SE_A_Java/app/src/main/java/com/example/pmd_se_a_java/GoogleMapAPI/MapsActivity.java
@@ -1,8 +1,18 @@
 package com.example.pmd_se_a_java.GoogleMapAPI;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.pmd_se_a_java.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -17,6 +27,9 @@ import com.example.pmd_se_a_java.databinding.ActivityMaps2Binding;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+
+    LocationManager locationManager;
+    LocationListener locationListener;
     private ActivityMaps2Binding binding;
 
     @Override
@@ -26,12 +39,43 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         binding = ActivityMaps2Binding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-    }
 
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                /*Log.d("TAG", location.toString());
+                LatLng obj = new LatLng(location.getLatitude(),location.getLongitude());
+                mMap.addMarker(new MarkerOptions().position(obj).title("NEW POSITION"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(obj, 7));*/
+                mapOnUserLocation(location, "User current Location");
+            }
+
+        };
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION}
+                    , 01);
+        }
+        else{
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            Location userLastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        }
+
+
+    }
+    public void mapOnUserLocation(Location location,String title)
+    {
+        LatLng UserLocation = new LatLng(location.getLatitude(),location.getLongitude());
+        mMap.addMarker(new MarkerOptions().position(UserLocation).title(title));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(UserLocation,10));
+
+    }
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -47,7 +91,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
 
         // Add a marker in Sydney and move the camera
-        LatLng address1 = new LatLng(31.6008316 ,73.0331767);
+        LatLng address1 = new LatLng(31.60592227 ,73.0364344);
         mMap.addMarker(new MarkerOptions().position(address1).title("Marker in Loonywala").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET)));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(address1, 5));
     }
